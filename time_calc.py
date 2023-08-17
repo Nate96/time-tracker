883ab1a319f846b8abbf213f18eb0546af5782eb
Nate96 <braukhoff96@gmail.com>
2023-08-17 17:17:24 -0500

    Added GUI

--- time_calc.py
+++ time_calc.py
@@ -1,39 +1,71 @@
 from datetime import datetime, timedelta
+from tkinter import *
+from tkinter import ttk
+import tkinter as tk
+
+NORMAL_MODE = 1
+FRIDAY_MODE = 2
 
 def display_results(hours):
-    print("Total hours worked today: {}".format(round(hours, 2)))
-    print("%50: {} \n%25: {}".format(round(hours / 2, 2), round(hours/4, 2)))
+    return f'Total Hours: {round(hours,2)} \n50%: {round(hours/2, 2)} \n25%: {round(hours/4, 2)}'
 
-print("----------------------")
-print("---- Time Tracker ----")
-print("----------------------")
+def nomral_mode():
 
-print("1 = Normal Mode")
-print("2 = Friday Mode")
-mode = int(input("Enter in Mode: "))
-
-if mode > 2 or mode < 1:
-    print("Invalid Mode")
-elif mode == 1:
-    start_time = str(input('Enter in Start time: ') + ":00")
-    end_time = str(input('Enter in End time: ') + ":00")
-
-    start_strip = datetime.strptime(start_time, "%H:%M:%S")
-    end_strip = datetime.strptime(end_time, "%H:%M:%S")
+    start_strip = datetime.strptime(start_time.get(), "%H:%M")
+    end_strip = datetime.strptime(end_time.get(), "%H:%M")
 
     delta = end_strip - start_strip
 
     sec = delta.total_seconds()
     hours = sec / (60 * 60)
-    
-    display_results(hours)   
-elif mode == 2:
-    total_time = float(input("Enter in total time worked for the week: "))
-    start_time = str(input("Enter in time started: ") + ":00")
+
+    Message(frm, text=display_results(hours)).grid(column=0, row=4, pady=5)
+
+
+
+def friday_mode():
+    total_time = total_hours_worked.get()
 
     remaining_hours = 40.0 - total_time
 
-    time_out = datetime.strptime(start_time, "%H:%M:%S") + timedelta(hours=remaining_hours)
+    time_out = datetime.strptime(start_time.get(), "%H:%M") + timedelta(hours=remaining_hours)
 
-    print("You get to work until: {}".format(time_out.time()))
-    display_results(remaining_hours)
+    Message(frm, text=f"End time {time_out.time()}\n" + display_results(remaining_hours)).grid(column=0, row=4, pady=5)
+
+if __name__ == '__main__':
+
+    root = tk.Tk()
+    root.title("Time Tracker")
+
+    start_time = tk.StringVar()
+    end_time = tk.StringVar()
+    total_hours_worked = tk.IntVar()
+    
+    frm = ttk.Frame(root, padding=10)
+    frm.grid()
+    ttk.Label(frm, text="Start Time:").grid(column=0, row= 0, pady=5)
+    ttk.Entry(frm, textvariable=start_time).grid(column=1, row=0, pady=5)
+    ttk.Label(frm, text="End Time:").grid(column=0, row= 1, pady=5)
+    ttk.Entry(frm, textvariable=end_time).grid(column=1, row=1, pady=5)
+    ttk.Label(frm, text="Total Hours:").grid(column=0, row= 2, pady=5)
+    ttk.Entry(frm, textvariable=total_hours_worked).grid(column=1, row=2, pady=5)
+    ttk.Button(frm, text="Normal", command=nomral_mode).grid(column=0, row=3, pady=5)
+    ttk.Button(frm, text="Friday Mode", command=friday_mode).grid(column=1, row=3,pady=5)
+
+    Message(frm, text="Out put").grid(column=0, row=4, pady=5)
+    root.mainloop()
+
+    # print("----------------------")
+    # print("---- Time Tracker ----")
+    # print("----------------------")
+
+    # print("1 = Normal Mode")
+    # print("2 = Friday Mode")
+    # mode = int(input("Enter in Mode: "))
+
+    # if mode > 2 or mode < 1:
+    #     print("Invalid Mode")
+    # elif mode == NORMAL_MODE:
+    #     nomral_mode()
+    # elif mode == FRIDAY_MODE:
+    #     friday_mode()
