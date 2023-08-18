fb37a86cdcfa5a708370d60d96e945368d33a97d
Nate96 <braukhoff96@gmail.com>
2023-08-18 11:11:58 -0500

    Cleaned code and added comments

--- time_calc.py
+++ time_calc.py
@@ -3,14 +3,18 @@ from tkinter import *
 from tkinter import ttk
 import tkinter as tk
 
-NORMAL_MODE = 1
-FRIDAY_MODE = 2
-
 def display_results(hours):
+    """
+    Displays hour distributed by charge codes
+    """
     return f'Total Hours: {round(hours,2)} \n50%: {round(hours/2, 2)} \n25%: {round(hours/4, 2)}'
 
 
 def nomral_mode():
+    """Normal Mode
+    Calculates total time (hours) by using a start time and an end time. 
+    Then, displays the results.
+    """
     start_strip = datetime.strptime(start_time.get(), "%H:%M")
     end_strip = datetime.strptime(end_time.get(), "%H:%M")
 
@@ -24,8 +28,11 @@ def nomral_mode():
 
 
 def friday_mode():
-    total_time = total_hours_worked.get()
-
+    """Friday Mode
+    Calculates time to leave and hours need to be worked on Friday. 
+    Then, Displays the results
+    """
+    total_time = float(total_hours_worked.get())
     remaining_hours = 40.0 - total_time
 
     time_out = datetime.strptime(start_time.get(), "%H:%M") + timedelta(hours=remaining_hours)
@@ -36,13 +43,13 @@ def friday_mode():
 if __name__ == '__main__':
     root = tk.Tk()
     root.title("Time Tracker")
-
-    start_time = tk.StringVar()
-    end_time = tk.StringVar()
-    total_hours_worked = tk.IntVar()
-    
     frm = ttk.Frame(root, padding=10)
     frm.grid()
+    
+    start_time = tk.StringVar()
+    end_time = tk.StringVar()
+    total_hours_worked = tk.StringVar()
+
     ttk.Label(frm, text="Start Time:").grid(column=0, row= 0, pady=5)
     ttk.Entry(frm, textvariable=start_time).grid(column=1, row=0, pady=5)
     ttk.Label(frm, text="End Time:").grid(column=0, row= 1, pady=5)
