2c1976a0d190551991b792caf5165257acaf11b0
Nate96 <braukhoff96@gmail.com>
2026-01-24 17:03:30 -0600

    refactored state

--- tt.py
+++ tt.py
@@ -5,6 +5,7 @@ import presenter
 import punch_clock
 
 from config import Res, MESSAGES
+from time_sheet import Punch
 
 if __name__ == '__main__':
     args = None
@@ -26,13 +27,20 @@ Actions:
   show month       Shows all entries for the month
                                 ''')
 
-    _ = parser.add_argument('action', help='refer to actions')
-    _ = parser.add_argument('comment', nargs='?', default='', help='strings that are tied to the punch')
+    parser.add_argument('action', help='refer to actions')
+    parser.add_argument('comment', nargs='?', default='', help='strings that are tied to the punch')
 
     # Parse all command line arguments
     args = parser.parse_args(args)
 
-    if args.action in ("p", "punch"): punch_clock.punch(args.comment)
+    if args.action in ("p", "punch"):
+        results = punch_clock.punch(args.comment)
+
+        if isinstance(results, Punch):
+            presenter.show_punch(results)
+        else:
+            presenter.show_entry(results)
+
     elif args.action == "show": presenter.show_entries(args.comment)
     elif args.action == "status": presenter.show_state()
     elif args.action == "report": presenter.report(args.comment)
