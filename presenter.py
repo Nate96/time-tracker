31589c0b39345deb59e3b5acbdb89c1843681a67
Nate96 <braukhoff96@gmail.com>
2026-02-10 20:17:15 -0600

    removed hours

--- presenter.py
+++ presenter.py
@@ -3,15 +3,15 @@ from datetime import datetime
 import time_sheet
 import punch_clock
 
-from config import TRACKER, PunchType, Res, DATE_FORMAT, MESSAGES
+from config import PunchType, Res, DATE_FORMAT, MESSAGES
 
 
 DATE_TIME_FORMAT   = "%Y-%m-%d %I:%M %p"
 DATE_TIME_FORMAT_2 = "%Y-%m-%d %I:%M %p"
 TIME_FORMAT        = "%I:%M %p"
 
-REPORT  = "======= Report ======="
-DIVIDER = "======================"
+REPORT  = "==== Report ===="
+DIVIDER = "================"
 
 
 def show_punch(punch: punch_clock.Punch):
@@ -26,7 +26,7 @@ def show_punch(punch: punch_clock.Punch):
 
 def show_entry(entry: punch_clock.Entry):
     """
-    {total} Hours (in out)
+    {total}  (in out)
     title
     comment
     """
@@ -36,7 +36,7 @@ def show_entry(entry: punch_clock.Entry):
     temp = datetime.strptime(f'{entry.out_punch}', DATE_FORMAT)
     out_formatted = temp.strftime(TIME_FORMAT)
 
-    print(f"{round(entry.total_time, 2)} Hours {in_formatted} - {out_formatted}")
+    print(f"{round(entry.total_time, 2)}  {in_formatted} - {out_formatted}")
     print(f"*{entry.title}*")
     print(entry.comment)
 
@@ -72,7 +72,7 @@ def show_entries(duration: str):
            print(f'{in_formatted:<20} {out_formatted:<10} {day:<10} {e.total_time:<6} {e.title:<{title_len}}: {e.comment:<30}')
            total += e.total_time
     
-       print(f'\nTotal: {total:.2f} hours')
+       print(f'\nTotal: {_convert_float_to_time(total)}')
 
 
 def show_last_punch(): show_punch(time_sheet.get_last_punch())
@@ -100,28 +100,28 @@ def report(duration: str) -> None:
             total_hours += float(entry.total_time)
 
         print(f"\n{REPORT}")
-        print(f'Sunday:    {_convert_float_to_time(week_hours[6])} Hours')
+        print(f'Sunday:    {_convert_float_to_time(week_hours[6])}')
         show_day_breakdown([e for e in entries if datetime.strptime(e.in_punch, DATE_FORMAT).weekday() == 6])
 
-        print(f'Monday:    {_convert_float_to_time(week_hours[0])} Hours')
+        print(f'Monday:    {_convert_float_to_time(week_hours[0])}')
         show_day_breakdown([e for e in entries if datetime.strptime(e.in_punch, DATE_FORMAT).weekday() == 0])
 
-        print(f'Tuesday:   {_convert_float_to_time(week_hours[1])} Hours')
+        print(f'Tuesday:   {_convert_float_to_time(week_hours[1])}')
         show_day_breakdown([e for e in entries if datetime.strptime(e.in_punch, DATE_FORMAT).weekday() == 1])
 
-        print(f'Wednesday: {_convert_float_to_time(week_hours[2])} Hours')
+        print(f'Wednesday: {_convert_float_to_time(week_hours[2])}')
         show_day_breakdown([e for e in entries if datetime.strptime(e.in_punch, DATE_FORMAT).weekday() == 2])
 
-        print(f'Thursday:  {_convert_float_to_time(week_hours[3])} Hours')
+        print(f'Thursday:  {_convert_float_to_time(week_hours[3])}')
         show_day_breakdown([e for e in entries if datetime.strptime(e.in_punch, DATE_FORMAT).weekday() == 3])
 
-        print(f'Friday:    {_convert_float_to_time(week_hours[4])} Hours')
+        print(f'Friday:    {_convert_float_to_time(week_hours[4])}')
         show_day_breakdown([e for e in entries if datetime.strptime(e.in_punch, DATE_FORMAT).weekday() == 4])
 
-        print(f'Saturday:  {_convert_float_to_time(week_hours[5])} Hours')
+        print(f'Saturday:  {_convert_float_to_time(week_hours[5])}')
         show_day_breakdown([e for e in entries if datetime.strptime(e.in_punch, DATE_FORMAT).weekday() == 5])
         print(DIVIDER)
-        print(f'Total:     {_convert_float_to_time(total_hours)} hours')
+        print(f'Total:     {_convert_float_to_time(total_hours)}')
         print(f'{show_total_breakdown(entries)}')
 
 
@@ -133,7 +133,7 @@ def show_total_breakdown(entries: list[punch_clock.Entry]) -> str:
         task_dict[ent.title] = task_dict.get(ent.title, 0.0) + ent.total_time
 
     for name, amount in task_dict.items():
-        output += f"* {name:<8} {_convert_float_to_time(amount)} hours\n"
+        output += f"* {name:<8} {_convert_float_to_time(amount)}\n"
 
     return output
 
@@ -150,13 +150,13 @@ def show_state():
 
             show_punch(state.last_punch)
             print('')
-            print(f'Session: {_convert_float_to_time(session_total)} hours')
+            print(f'Session: {_convert_float_to_time(session_total)}')
         else:
             show_entry(state.last_entry)
             print('')
 
-        print(f'Day:     {_convert_float_to_time(session_total + state.get_day_total())} hours')
-        print(f'Week:    {_convert_float_to_time(session_total + state.get_week_total())} hours')
+        print(f'Day:     {_convert_float_to_time(session_total + state.get_day_total())}')
+        print(f'Week:    {_convert_float_to_time(session_total + state.get_week_total())}')
     else:
         print(Res.NO_DB.value)
 
@@ -169,7 +169,7 @@ def show_day_breakdown(entries: list[punch_clock.Entry]) -> None:
 
         output = ""
         for name, amount in tasks.items():
-            output += f"* {name:<8} {_convert_float_to_time(amount)} hours\n"
+            output += f"* {name:<8} {_convert_float_to_time(amount)}\n"
 
         print(output)  # Remove last newline
 
